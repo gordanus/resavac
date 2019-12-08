f8107ac80d07a918bf7bd468b0868911f440bdca
Gordan Blazic <gordanus@gmail.com>
2019-12-08 23:51:17 +0100

    add preloader

--- script.js
+++ script.js
@@ -81,6 +81,29 @@ $(document).ready(function () {
         }
     });
 
+/* Preloader
+    * -------------------------------------------------- */
+   var clPreloader = function() {
+        
+    $("html").addClass('cl-preload');
 
+    $(window).on('load', function() {
+
+        //force page scroll position to top at page refresh
+        $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+        // will first fade out the loading animation 
+        $("#loader").fadeOut("slow", function() {
+            // will fade out the whole DIV that covers the website.
+            $("#preloader").delay(300).fadeOut("slow");
+        }); 
+        
+        // for hero content animations 
+        $("html").removeClass('cl-preload');
+        $("html").addClass('cl-loaded');
+    
+    });
+};
+clPreloader();
 
 });
